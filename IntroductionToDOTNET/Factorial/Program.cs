a61c4f27ba6bd70b53103128dc69fca9217e05af
User <User@DESKTOP-0TCDSR6>
2024-09-09 19:40:58 +0300

    BigInt Factorial

---
+++ IntroductionToDOTNET/Factorial/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Numerics;
+
+namespace Factorial
+{
+	class Program
+	{
+		static void Main(string[] args)
+		{
+			try
+			{
+				Console.Write("Введите число для вычисления факториала: ");
+				int n = Convert.ToInt32(Console.ReadLine());
+				BigInteger f = 1;
+				for (int i = 1; i <= n; i++)
+				{
+					f *= i;
+					Console.WriteLine($"{i}! = {f}");
+				}
+			}
+			catch (Exception ex)
+			{
+				Console.WriteLine(ex.Message);
+			}
+		}
+	}
+}
