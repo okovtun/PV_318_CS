903831a8c334b0af5b9e573828cc5cbc2cfbddbf
User <User@DESKTOP-0TCDSR6>
2024-09-13 19:30:19 +0300

    Arrays DONE

--- IntroductionToDOTNET/Arrays/Program.cs
+++ IntroductionToDOTNET/Arrays/Program.cs
@@ -1,5 +1,5 @@
 ﻿//#define ARRAYS_1
-//#define ARRAYS_2
+#define ARRAYS_2
 #define JAGGED_ARRAYS
 using System;
 using System.Collections.Generic;
@@ -40,11 +40,11 @@ namespace Arrays
 			Console.WriteLine();
 
 			//Range-based for
-			foreach (int i in arr)
+			/*foreach (int i in arr)
 			{
 				Console.Write(i + "\t");
 			}
-			Console.WriteLine();
+			Console.WriteLine();*/
 			/*
 			--------------------------
 			foreach(Type iterator in collection)
@@ -53,13 +53,18 @@ namespace Arrays
 				........;
 			}
 			--------------------------
-			 */ 
+			 */
+			Console.WriteLine($"Сумма элементов массива: {arr.Sum()}");
+			Console.WriteLine($"Среднее-арифметическое элементов массива: {arr.Average()}");
+			Console.WriteLine($"Минимальное значение в массиве: {arr.Min()}");
+			Console.WriteLine($"Максимальное значение в массиве: {arr.Max()}");
 #endif
 
 #if ARRAYS_2
 			//int[,] i_arr_2 = new int[3,4];
 			int[,] i_arr_2 = new int[,]
 				{
+					{ 0, 1, 1, 2 },
 					{ 3, 5, 8, 13},
 					{ 21, 34, 55, 89},
 					{ 144, 233, 377, 610}
@@ -81,12 +86,16 @@ namespace Arrays
 				Console.WriteLine();
 			}
 			Console.WriteLine();
+			Console.WriteLine($"Сумма элементов массива: {i_arr_2.Cast<int>().Sum()}");
+			Console.WriteLine($"Среднее-арифметическое элементов массива: {i_arr_2.Cast<int>().Average()}");
+			Console.WriteLine($"Минимальное значение в массие: {i_arr_2.Cast<int>().Min()}");
+			Console.WriteLine($"Максимальное значение в массие: {i_arr_2.Cast<int>().Max()}");
 
-			foreach(int i in i_arr_2)
+			/*foreach(int i in i_arr_2)
 			{
 				Console.Write(i + "\t");
 			}
-			Console.WriteLine();
+			Console.WriteLine();*/
 #endif
 
 #if JAGGED_ARRAYS
@@ -95,7 +104,7 @@ namespace Arrays
 					new int[]{ 0, 1, 1, 2},
 					new int[]{ 3, 5, 8, 13, 21},
 					new int[]{ 34, 55, 89},
-					new int[]{ 144, 233, 377, 610, 987},
+					new int[]{ 144, 233, 377, 610},
 				};
 			for (int i = 0; i < j_arr.Length; i++)
 			{
@@ -105,6 +114,22 @@ namespace Arrays
 				}
 				Console.WriteLine();
 			}
+			//Console.WriteLine($"Сумма элементов массива: {j_arr.Cast<int>().Sum()}");
+			int sum = 0;
+			int count = 0;
+			int min, max;
+			min = max = j_arr[0][0];
+			for (int i = 0; i < j_arr.Length; i++)
+			{
+				sum += j_arr[i].Sum();
+				count += j_arr[i].Length;
+				if (j_arr[i].Min() < min) min = j_arr[i].Min();
+				if (j_arr[i].Max() > max) max = j_arr[i].Max();
+			}
+			Console.WriteLine($"Сумма элементов массива: {sum}");
+			Console.WriteLine($"Среднее-арифметическое элементов массива: {(double)sum/count}");
+			Console.WriteLine($"Минимальное значение в массиве: {min}");
+			Console.WriteLine($"Максимальное значение в массиве: {max}");
 #endif
 		}
 	}
