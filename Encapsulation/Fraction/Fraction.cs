1c2b178cc4adece6f5a2cb2fd7ec24612c42f7d7
User <User@DESKTOP-0TCDSR6>
2024-09-16 21:39:06 +0300

    Operator * DONE

--- Encapsulation/Fraction/Fraction.cs
+++ Encapsulation/Fraction/Fraction.cs
@@ -8,7 +8,7 @@ namespace Fraction
 {
 	class Fraction
 	{
-		public int Integer { get; set; }	//Автосвойства, которые не требую объявления переменной, поскольку сами, неявно ее объявляют.
+		public int Integer { get; set; }    //Автосвойства, которые не требую объявления переменной, поскольку сами, неявно ее объявляют.
 		public int Numerator { get; set; }
 		/// ////////////////////
 		int denominator;
@@ -22,27 +22,93 @@ namespace Fraction
 		/// 
 
 		//			  Constructors:
+		public Fraction()
+		{
+			Console.WriteLine($"DefConstructor:\t{GetHashCode()}");
+		}
 		public Fraction(int integer)
 		{
 			this.Integer = integer;
 			Console.WriteLine($"1ArgConstructor:{GetHashCode()}");
 		}
+		public Fraction(int numerator, int denominator)
+		{
+			this.Numerator = numerator;
+			this.Denominator = denominator;
+			Console.WriteLine($"Constructor:\t{GetHashCode()}");
+		}
+		public Fraction(int integer, int numerator, int denominator)
+		{
+			this.Integer = integer;
+			this.Numerator = numerator;
+			this.Denominator = denominator;
+			Console.WriteLine($"Constructor:\t{GetHashCode()}");
+		}
+		public Fraction(Fraction other)
+		{
+			this.Integer = other.Integer;
+			this.Numerator = other.Numerator;
+			this.Denominator = other.Denominator;
+			Console.WriteLine($"CopyConstructor:{GetHashCode()}");
+		}
 		~Fraction()
 		{
-			Console.WriteLine($"Destructor:{GetHashCode()}");
+			Console.WriteLine($"Destructor:\t{GetHashCode()}");
 		}
+
+		//			   Operators:
+		public static Fraction operator *(Fraction l, Fraction r)
+		{
+			Fraction left = l.Improper();
+			Fraction right = r.Improper();
+			Fraction res = new Fraction
+				(
+				left.Numerator * right.Numerator,
+				left.Denominator * right.Denominator
+				);
+			return res;
+		}
+
 		//				Methods:
+		Fraction Proper()
+		{
+			Fraction copy = new Fraction(this);
+			copy.Integer +=   copy.Numerator / copy.Denominator;
+			copy.Numerator %= copy.Denominator;
+			return copy;
+		}
+		Fraction Improper()
+		{
+			Fraction copy = new Fraction(this);
+			copy.Numerator += copy.Integer * copy.Denominator;
+			copy.Integer = 0;
+			return copy;
+		}
 		public void Print()
 		{
 			if (Integer != 0) Console.Write(Integer);
 			if (Numerator != 0)
 			{
-				Console.Write("(");
+				if (Integer != 0) Console.Write("(");
 				Console.Write($"{Numerator}/{Denominator}");
-				Console.Write(")");
+				if (Integer != 0) Console.Write(")");
 			}
 			else if (Integer == 0) Console.Write(0);
 			Console.WriteLine();
+		}
+		public override string ToString()
+		{
+			//return base.ToString();
+			string print = "";
+			if (Integer != 0) print += Integer;
+			if (Numerator != 0)
+			{
+				if (Integer != 0) print += "(";
+				print += $"{Numerator}/{Denominator}";
+				if (Integer != 0) print += ")";
+			}
+			else if (Integer == 0) print += 0;
+			return print;
 		}
 	}
 }
--- Encapsulation/Fraction/Program.cs
+++ Encapsulation/Fraction/Program.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿//#define CONSTRUCTORS_CHECK
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -10,11 +11,31 @@ namespace Fraction
 	{
 		static void Main(string[] args)
 		{
+#if CONSTRUCTORS_CHECK
 			Fraction A = new Fraction();
 			A.Print();
+			Console.WriteLine(A);
 
 			Fraction B = new Fraction(5);
 			B.Print();
+			Console.WriteLine(B);
+
+			Fraction C = new Fraction(1, 2);
+			C.Print();
+			Console.WriteLine(C);
+
+			Fraction D = new Fraction(2, 3, 4);
+			D.Print();
+			Console.WriteLine(D.ToString());   //ToString()
+			Console.WriteLine(D.GetType()); 
+#endif
+
+			Fraction A = new Fraction(2, 3, 4);
+			Fraction B = new Fraction(3, 4, 5);
+			Fraction C = new Fraction(A * B);
+			A.Print();
+			B.Print();
+			C.Print();
 		}
 	}
 }
