c13b5ae5cff121fbace6bc7dcc303572ccea7206
User <User@DESKTOP-0TCDSR6>
2024-09-16 19:44:15 +0300

    Operators overloading in progress

---
+++ Encapsulation/Encapsulation/Point.cs
@@ -0,0 +1,97 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Encapsulation
+{
+	class Point
+	{
+		/*double x;
+		double y;
+		public double X
+		{
+			get
+			{
+				return x;
+			}
+			set
+			{
+				//if(value...)......
+				x = value;	//value - открывает доступ к переданному значению
+			}
+		}
+		public double Y
+		{
+			get { return y; }
+			set { y = value; }
+		}
+
+		public double GetX()
+		{
+			return x;
+		}
+		public double GetY()
+		{
+			return y;
+		}
+		public void SetX(double x)
+		{
+			this.x = x;	//В языке C# отсутствуют указатели, а следовательно отсутсвует оператор '->'
+		}
+		public void SetY(double y)
+		{
+			this.y = y;
+		}*/
+
+		public double X { get; set; }   //Автосвойства для 'X'
+		public double Y { get; set; }   //Автоствойства для 'Y'
+
+		//			  Constructors:
+		public Point(double x = 0, double y = 0)
+		{
+			X = x;
+			Y = y;
+			Console.WriteLine($"Constructor:\t{this.GetHashCode()}");
+		}
+		public Point(Point other)
+		{
+			this.X = other.X;
+			this.Y = other.Y;
+			Console.WriteLine($"CopyConstructor:{this.GetHashCode()}");
+		}
+		~Point()
+		{
+			Console.WriteLine($"Destructor:\t{this.GetHashCode()}");
+		}
+
+		//				Operators:
+		public static Point operator +(Point left, Point right)
+		{
+			Point res = new Point();
+			res.X = left.X + right.X;
+			res.Y = left.Y + right.Y;
+			return res;
+		}
+
+		//				Methods:
+		public void Print()
+		{
+			Console.WriteLine($"X = {X}, Y = {Y};");
+		}
+	}
+}
+
+/*
+-----------------------
+private
+public
+protected
+internal
+protected internal
+-----------------------
+Properties
+value
+-----------------------
+ */
---
+++ Encapsulation/Encapsulation/Program.cs
@@ -0,0 +1,55 @@
+﻿//#define CONSTRUCTORS_CHECK
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Encapsulation
+{
+	class Program
+	{
+		static void Main(string[] args)
+		{
+#if CONSTRUCTORS_CHECK
+			//Stack - статическая память;
+			//Heap - динамическая память;
+
+			//Point A = new Point();  //Оператор 'new' вызывает конструктор класса.
+			//A.SetX(2);
+			//A.SetY(3);
+			//Console.WriteLine($"X = {A.GetX()}, Y = {A.GetY()};");
+			//A.Print();
+
+			Point B = new Point();  //Default constructor
+			B.X = 7;
+			B.Y = 8;
+			B.Print();
+
+			Point C = new Point(11, 12);
+			C.Print();
+
+			//Console.WindowWidth;
+			//Console.SetWindowSize
+
+			Point D = C;    //Здесь копируется НЕ объект, а ссылка на него
+			D.Print();
+			D.X = 111;
+			C.Print();
+
+			//Объекты копируются следующим образом:
+			Point E = new Point(D); //Здесь вызывается конструктор копирования
+			E.Print();
+			E.X = 11;
+			D.Print();
+
+			E = new Point(B);
+			E.Print(); 
+#endif
+			Point A = new Point(2, 3);
+			Point B = new Point(7, 8);
+			Point C = new Point(A + B);
+			C.Print();
+		}
+	}
+}
