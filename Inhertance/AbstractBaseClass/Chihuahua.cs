db1d59ce933c709f196541ed1e2d685fa7213039
User <User@DESKTOP-0TCDSR6>
2024-09-20 21:29:22 +0300

    Abstract base class

---
+++ Inhertance/AbstractBaseClass/Chihuahua.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AbstractBaseClass
+{
+	class Chihuahua:Dog
+	{
+		public override void Sound()
+		{
+			Console.WriteLine("Недособака тяф, тяф....."); ;
+		}
+	}
+}
---
+++ Inhertance/AbstractBaseClass/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AbstractBaseClass
+{
+	class Program
+	{
+		static void Main(string[] args)
+		{
+			//Animal animal = new Animal();	//Создать экземпляр абстрактного класса невозможно.
+			Wolf wolf = new Wolf();
+			wolf.Sound();
+
+			Chihuahua nedosobaka = new Chihuahua();
+			nedosobaka.Sound();
+		}
+	}
+}
